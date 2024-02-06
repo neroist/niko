2e5b74a5d5e83ba7a7eff960d1fcbe3862f312b5
neroist <pmpiga1@gmail.com>
2024-02-06 00:26:32 -0500

    add mukalamasin

---
+++ toki/mukalamasin.nim
@@ -0,0 +1,297 @@
+import nimib
+
+template addCss*(doc: var NbDoc; style: string) =
+  doc.context["nb_style"] = doc.context["nb_style"].vString &
+    '\n' & "<style>\n" & style & "\n</style>"
+
+nbInit 
+
+nb.darkMode()
+nb.title = "mi kama kute e mukalamasin"
+
+nb.addCss: """
+figure { text-align: center; }
+"""
+
+nbText: """
+# mi kama kute e mukalamasin
+
+tenpo suli suli mute mute la mi awen kute e kalama sama taso • taso mi kama
+kute e kalama sin • ni li nasa • li pona a • kalama sin li pona tawa mi tawa
+kute a • mi pana e kalama sin mi tawa sina a • 
+
+kin ante la tenpo lon la mi wile lape • lawa mi li wile pini e lukin mi li
+wile lape e mi • mi alasa utala e wile ni lon tenpo ni • ken la mi toki nasa
+anu sitelen nasa • taso a kalama o pana
+
+---
+
+## mu [Pathological Facade]
+
+<https://www.youtube.com/watch?v=inRXBlYgMGI> kalama ni li tan jan? kulupu?
+msa • kalama li tan ijo a [Ghost and Pals] li pona mute • nasin kalama la
+ona li kepeken nasin 7/8 !!! • ni li nasa li pona a tawa mi • kin sitelen
+a li pona mute a!!! li suwii
+"""
+
+nbImage(
+  "https://raw.githubusercontent.com/neroist/poki/3dab6506f02f7b797bebb1df1da6c8594565f874/sitelen/pfteto2.png",
+  "",
+  "ilo Jutu la sitelen ni"
+)
+
+nbText: """
+
+---
+
+## kalama kulupu tu wawa
+
+kalama tu kama li tan jan sama la mi kulupu e ona ali
+
+ona ali li tan jan [biz] li tan jan [ZERA] li tan jan [LOLUET] • jan [biz]
+en jan [ZERA] li pali e nimi kalama e (ijo ante kalama kin) • jan [LOLUET]
+li mu suwi e nimi lon kalama • kalama tu ni la ona ali li kama kulupu li
+pali wawa a
+
+### mu [Loveit?]
+
+<https://www.youtube.com/watch?v=uXZkGWbyvcM> pilin mi la kalama sin ali
+la kalama ni li pona nanpa wan a • kalama en sitelen kin a li wawa a • o
+kute o lukinn
+"""
+
+nbImage(
+  # suli... taso mi alasa lili e linja la sitelen li kama pakala lon lipu
+  "https://tcj-image-production.s3.ap-northeast-1.amazonaws.com/u123060/r1009707/itew1009707.webp?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJV4VIS2VN3X4CBGA%2F20240206%2Fap-northeast-1%2Fs3%2Faws4_request&X-Amz-Date=20240206T035252Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=67f8495256f0878ecf1b26b93a03577b5dab8205de0bf08b67315eac6ccf54ed",
+  "",
+  "mu ni la sitelen ona"
+)
+
+nbText: """
+### mu [Lovecat]
+
+<https://www.youtube.com/watch?v=hrNHsbYjFNg> soweli li utala e jan li alasa
+lawa e jan • taso soweli li suwi mute mute a liiii smndfjsiknfsj • **~~ona o
+lawa e mi kin~~**
+"""
+
+nbImage(
+  "https://raw.githubusercontent.com/neroist/poki/a10423793c51bbdd001de9837c6f1fae8b0d0a79/sitelen/lovecat4.png",
+  "",
+  "ona"
+)
+
+nbImage(
+  "https://raw.githubusercontent.com/neroist/poki/a10423793c51bbdd001de9837c6f1fae8b0d0a79/sitelen/lovecat8.png",
+  "",
+  "ona"
+)
+
+nbText: """
+
+mi wile e ona aaaaasnfjdskngjhkngfdkgn
+
+---
+
+## wawa nanpa wan ala wan [hya]
+
+mi wile toki e jan [hya] a • ona li pali e kalama tu ni • lukin la ona li
+suli ala li sin a • ona o suli a! • o kute e kalama ona o pana e pona tawa
+ona!
+
+### mu [snake]
+
+nanpa wan la <https://www.youtube.com/watch?v=nvOHyTucpdg> <- ni a li pona
+pona pona mute a tawa mi • sitelen en kalama kin li monsuta li wawa • kin
+open la sitelen tawa li alasa monsuta e sina • ona li pana e sitelen monsuta
+tawa sina lon tenpo lili • sina sona ala e kama sitelen • o monsuta tan ona
+• pilin mi la ni li monsuta e jan • taso ona li monsuta ala e mi a • nasa
+ala nasa a
+"""
+
+nbImage(
+  "https://raw.githubusercontent.com/neroist/poki/000d10647878aacb74db3cc91996160f7ba6aef5/sitelen/hsnake4.png",
+  "",
+  "ilo Jutu la sitelen ni"
+)
+
+nbText: """
+
+### mu [Sasha_meeting]
+
+nanpa tu la <https://www.youtube.com/watch?v=dTAdu6N4p9Y> kalama ni la ona
+li awen ala li open kepeken tenpo lili lili a • ni li nasa a • a kin mi
+ken sona lili e toki • jan li pali e kalama la ona li toki e ijo kepeken
+kalama • ken la ona li toki e pilin anu sona anu ijo ante a • mi sona e
+toki la  ni li nasa suli a • mute mute la kalama li toki a e ijo • taso mi
+ken ala sona e toki ona • mute la kalama li pona kute taso
+"""
+
+nbImage(
+  "https://i.ytimg.com/vi/dTAdu6N4p9Y/hqdefault.jpg",
+  "",
+  "ilo Jutu la sitelen ni li sitelen sinpin"
+)
+
+nbText: """
+
+---
+
+## kalama ante a
+
+kalama ante mute a pona li lon a • taso mi wile ala pali mute la mi ken ala
+toki lon ona ali v taso mi ken pana e nasin kute a • o kute a!!
+"""
+
+# TODO make this into nimib later
+nbRawHtml: """
+  <div>jan Y kulupu -&gt;</div>
+  <div>
+    <br>
+  </div>
+  <div>kulupu monsuta LadyMonsters <br>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <a href="https://www.youtube.com/@LadyMonsters" target="_self">https://www.youtube.com/@LadyMonsters</a>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/6aQRHGTQomCAfEUMSCpCdj?utm_source=generator" width="100%" height="352" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
+      <br />
+    </iframe>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>wan x髥莏</div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <a href="https://www.youtube.com/@user-rx2ov9kt3y" target="_self">https://www.youtube.com/@user-rx2ov9kt3y</a>
+  </div>
+  <div>
+    <br>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/6tOzIqlpQBKAFIqdhmTfiW?utm_source=generator" width="100%" height="352" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>jan Azari</div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <a href="https://www.youtube.com/@xxxAzari" target="_self">https://www.youtube.com/@xxxAzari</a>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/40vWepxni4pplb4FdLDp2m?utm_source=generator" width="100%" height="352" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <br>
+    <br>‍ x0o0x_
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <a href="https://www.youtube.com/@x0o0x_" target="_self">https://www.youtube.com/@x0o0x_</a>
+  </div>
+  <div>
+    <br>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/2QLccJ5Jtn9Cj5xcfB9ufz?utm_source=generator" width="100%" height="352" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <br>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>lili よこしま</div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <a href="https://www.youtube.com/@454ma" target="_self">https://www.youtube.com/@454ma</a>
+  </div>
+  <div>
+    <br>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/1JZPFJUjJIzPAmjrEkdy46?utm_source=generator" width="100%" height="352" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <br>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>wawa [pupa]</div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <a href="https://www.youtube.com/channel/UCu3QrROcutyH7b9_gG7By5g" target="_self">https://www.youtube.com/channel/UCu3QrROcutyH7b9_gG7By5g</a>
+  </div>
+  <div>
+    <br>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/3V3gXQdKZsUvJfaWNnuvox?utm_source=generator" width="100%" height="352" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <br>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>wawa kin&nbsp;MeguriMeguruP </div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <a href="https://www.youtube.com/@MeguriMeguru_p" target="_self">https://www.youtube.com/@MeguriMeguru_p</a>
+  </div>
+  <div>
+    <br>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/37gq7KFHsAIEuEuRFT8RNl?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>    <br>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>suli FLG4</div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <a href="https://www.youtube.com/@flg4611" target="_self">https://www.youtube.com/@flg4611</a>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/63MKBngdiEGvCJhGfCDjMD?utm_source=generator" width="100%" height="352" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    <br>
+    <br>weka Abu-Se-Ken
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>
+    <a href="https://www.youtube.com/@abu-se-ken2852" target="_self">https://www.youtube.com/@abu-se-ken2852</a>
+  </div>
+  <div>
+    <br>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/3YIQKMfnZY6lNTDNUJ7wUX?utm_source=generator" width="100%" height="352" frameborder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>kin sijelo ni kin li lon? -&gt; <a href="https://open.spotify.com/artist/7DU4gbpf1tr6CZryceTtBx" target="_self">https://open.spotify.com/artist/7DU4gbpf1tr6CZryceTtBx</a>
+  </div>
+  <div>tan seme a www • ona li ante seme tan sijelo ante <br>
+  </div>
+  <div>
+    <br>
+  </div>
+  <div>pini lipu! mu <br>
+  </div>
+"""
+
+nbSave
