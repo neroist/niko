4c5acd9c4fd9d1b2d59f3a684887b840809294db
neroist <pmpiga1@gmail.com>
2024-02-05 23:15:54 -0500

    kala tonsi mi a

---
+++ toki/kalatonsi.nim
@@ -0,0 +1,49 @@
+import nimib
+
+nbInit 
+
+nb.darkMode()
+nb.title = "kala li tonsi la "
+
+nbText: """
+# kala li tonsi la
+
+tenpo poka la mi tawa kulupu esun [ikea] li tawa
+[lipu esun ona](https://www.ikea.com/us/en/) kepeken ilo mi • lipu la mi
+pana e mani tawa kulupu lon esun • mi esun e seme? • mi pana e mani • taso
+seme li kama lon mi tan pana? • o lukin a
+"""
+
+nbImage(
+  "https://raw.githubusercontent.com/neroist/poki/main/sitelen/blahaj2.png",
+  "KALA A",
+  "sitelen ni la kala tonsi suwi pona wawa musi laso mu mi"
+)
+
+nbText: """
+KALA TONSI
+
+KALA TONSIIIIIII • ALI LA TENPO ALI A LA MI WILE E ONAAAA • TENPO NI LA
+ONA LI LON MIIIIIIIIIIII AAAAAAAAAAAAAAAAAA • mi olin e onaaa • mi esun e
+mani $30 (anu mani sama) tawa onaa • ona li suwi li pona li ko li
+waaaadnhfsnfhskdnsnbfhjk • mi lape la ona li lon luka mi li pilin e mi •
+mi ken lape pona lon poka onaaaa • muu
+
+mi olin wawa e ona • taso mi pilin monsuta tan ijo poka a tan ijo ike ken •
+ken la mama mi li sona e kala ni e tonsi ona e meli ona (kala ni li lon meli
+tonsi mute) • mama li pilin ike suli tawa meli tawa tonsi • ona li sona la
+ken la ona li weka anu pakala e kala :( • ni li ike ali
+
+sin -> lukin la ona li sona ala e meli e tonsi li pakala ala e kala • pona a
+
+kin sona sin li kama lon mi -> tenpo la kala li ken kama lili sama ni • ni li
+ike tawa mi • ona o awen suli a :( • mi wile ala e ona lili • kin ona li kama
+lili la mi ken suli sin e ona kepeken nasin seme a? • jan li suli e kala ona
+kepeken ko walo kepeken ilo ante kin • taso ijo ali ni li lon ala mi a • ken
+mute la mi lukin alasa e ijo la ona li awen weka li kama ala lon mi a • ni li
+ike suli • kin kala li lili la pona lape kin li kama lili :(
+
+tan ijo tu ni la mi pilin monsuta • taso mi awen olin wawa e kala tonsi miiii • mu
+
+> *:3c*
+"""
