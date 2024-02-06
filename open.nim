73447237e4b8230a939e4b4462fe6a28c7b5ec49
neroist <pmpiga1@gmail.com>
2024-02-05 22:52:44 -0500

    first commit

---
+++ open.nim
@@ -0,0 +1,35 @@
+import nimib
+
+nbInit 
+
+nb.darkMode()
+
+nbText: """
+# lipu
+
+ni li lipu
+
+sina lukin e ona
+
+## sina pali e lipu tan seme
+
+tenpo pini la mi kepeken lipu [spacehey]
+
+taso lipu ni li ike
+
+mi ken ala poki e toki mi lon ona
+
+ni la mi ante e lipu kepeken
+
+mi kama pali e lipu ni
+
+  li kama kepeken ona
+
+## toki mi
+
+[mi pana lon tenpo kama]
+"""
+
+nb.filename = "index.html"
+
+nbSave
---
+++ toki/alasasona.nim
@@ -0,0 +1,79 @@
+import nimib
+
+nbInit 
+
+nb.darkMode()
+nb.title = "alasa sona [nanpa]"
+
+nbText: """
+# alasa sona `[nanpa]`
+
+ma sina la ali li ike • kulupu wan li lawa e jan ale e ma ale • utala suli
+li lon • taso sina en kulupu sina li ken weka e lawa • li ken kama e pona •
+taso a ike la kulupu lawa ike ni li lanpan e sina li awen e sina lon tomo
+awen • ona li wile pana e sina tawa tomo musi kin • tomo ante ni la kulupu
+ni li wile musi moli e sina • musi ni la sina ken kama moli • anu sijelo
+sina li ken kama pakala suli • taso a tenpo lon la sina lon tomo ni ala li
+lon tomo awen
+
+tomo awen ni la nasin mute li lon • nanpa li lon nasin ali • sina ken kama weka tan tomo kepeken nasin • taso ijo wawa li lon open nasin li lon pini kin li mu mute • tan ijo ni la sina ken ala kepeken nasin • taso lon poka nasin la ilo nanpa li lon • 1 en 2 en 3 en 4 en 5 en 6 en 7 en 8 en 9 en nanpa ante ala li lon ilo ni • sina ken pana e nanpa tu wan tawa ilo ni • nanpa ali sina li pona la ijo wawa nasin li kama weka • sina kama ken kepeken nasin • taso sina sona ala e nanpa pona
+
+taso a! sina kama sona e ijo sin: kulupu sina la kulupu lawa li pana e ken weka tawa jan wan • ken weka la jan ni li ken kama sona e nanpa pona li ken kama weka tan tomo awen • taso tenpo suno kama la kulupu lawa li pana e jan ali ante tawa monsuta suli • jan li kama moku pona a tawa monsuta ni
+
+kulupu sina la jan Sana li lon li ken pali pona pona a kepeken sona li sona mute • tan ni la kulupu sina li pana e ona tawa kulupu lawa • kulupu lawa li pana e ken weka tawa ona • jan lawa wan li weka e ijo wawa tan nasin • ona en jan Sana li tawa tomo ante kepeken nasin • ona li toki tawa jan Sana • taso sina pana e ilo kute len tawa jan Sana • ilo ni li kute e kalama li pana e ona tawa sina • tan ni la sina ken kute e toki • jan li toki tawa jan Sana
+
+sina kute
+
+「nasin ali la ilo nanpa li lon poka • sina pana e nanpa tu wan pona tawa ilo nanpa la ijo wawa li kama kon li ken e weka sina • mi pana e sona nanpa」
+
+「kulupu nanpa la nanpa #1 o lili nanpa wan • nanpa #2 o sama nanpa #1 anu suli tawa ona • nanpa #3 o suli nanpa wan anu sama nanpa #2 • nanpa 0 la nanpa ali o suli」
+
+「mi ken toki e sona namako tu wan taso tawa sina • sina sona e nanpa pona ali la o toki e nanpa • nanpa li pona la sina ken weka tan tomo • taso a nanpa li ike anu sina toki ala e nanpa li mu e ijo ante la mi pana e sina tawa tomo awen li weka e ken weka sina」
+
+jan Sana li toki 
+
+「o toki e sona namako nanpa wan」
+
+jan ante li toki
+
+「kulupu nanpa la sina unpa e nanpa ali la nanpa 36 li kama tan ni • sina sona ala la mi toki e nasin • 
+
+mi toki e ijo wan • sina unpa e nanpa 3 e nanpa 2 e nanpa 7 la linja [3 × 2 × 7] li kama lon • sina nanpa wan e linja ni la nanpa 42 li kama •
+
+kulupu nanpa la sina unpa e nanpa ali ona li nanpa wan e linja la nanpa 36 li kama lon」
+
+jan Sana li kama toki sin
+
+「o toki e sona nanpa tu」
+
+jan ante kin li kama toki
+
+「mi toki • kepeken nasin la sina kama lon tomo ni • nanpa li lon nasin ali • kulupu nanpa la sina wan e nanpa ali ona la nanpa li kama • nanpa ni li sama nanpa pi nasin sina」
+
+kalama ala li kama tan ilo sina lon tenpo suli • jan Sana li sona e nanpa nasin ona • taso sina sona ala e nanpa • kin jan Sana li ken ala toki e nanpa ona • ike ike a! 
+
+sina toki tawa sina lon lawa sina •「jan Sana li sona e nanpa pona ali kepeken sona tu ona ni la ona li toki e nanpa tawa jan ante li kama weka • taso jan Sana li toki ala e nanpa」
+
+sina kute e kalama sin tan ilo • jan Sana li mu li wile e sona nanpa tu wan
+
+jan ante li kama pana e sona ni
+
+「kulupu nanpa la nanpa wan taso li suli nanpa wan」
+
+tenpo lili la sina kute e pini mu pi ijo wawa nasin • jan Sana li kama weka • (taso sina kute ala e toki nanpa ona... nasa a) • kin a ike la ilo sina li kama ken ala kute • jan Sana li weka la ona li toki e nanpa pona la sina ken ala kute e ni • ike • sona tu tu li lon sina • sina ken kepeken sona ni taso 
+
+
+sona ali sina ->
+
+    sina unpa e nanpa ali la nanpa 36 li kama tan ni
+    kulupu nanpa la sina wan e nanpa ali ona la nanpa li kama • nanpa sama li lon nasin pi jan Sana
+    kulupu nanpa la nanpa wan taso li suli nanpa wan
+    jan Sana li wile e sona tu wan tawa sona nanpa 
+
+
+o alasa sona: nanpa tu wan pona li lon kulupu nanpa • nanpa ali ni li seme?
+
+(alasa ni li tan ala mi li tan sitelen [https://youtu.be/7Vd1dTBVbFg])
+
+
+"""
