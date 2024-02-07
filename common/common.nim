a381800d421ac390c4bf8f88c53593cea54d0bc1
neroist <pmpiga1@gmail.com>
2024-02-07 15:28:09 -0500

    o open e sitelen pona

---
+++ common/common.nim
@@ -0,0 +1,39 @@
+import std/strutils
+
+import nimib
+
+const
+  spCss* = """
+@font-face {
+  font-family: "sitelen seli kiwen mono asuki"
+  src: url(fonts/sitelenselikiwenmonoasuki.ttf)
+}
+
+main {
+  font-size: larger;
+  font-family: "sitelen seli kiwen mono asuki" !important;
+}
+"""
+
+template addCss*(doc: var NbDoc; style: string) =
+  doc.context["nb_style"] = doc.context["nb_style"].vString &
+    '\n' & "<style>\n" & style & "\n</style>"
+
+template nbTextSp*(text: string) = 
+  let txt = text
+              .replace("zz", "　") # better spaces
+
+              .replace("niv", "ni") # better multidirectional ni omg
+              .replace("ni>", "ni2")
+              .replace("ni^", "ni3")
+              .replace("ni<", "ni4")
+              .replace("niv>", "ni5")
+              .replace("ni>v", "ni5")
+              .replace("ni^>", "ni6")
+              .replace("ni>^", "ni6")
+              .replace("ni^<", "ni7")
+              .replace("ni<^", "ni7")
+              .replace("niv<", "ni8")
+              .replace("ni<v", "ni8")
+
+  nbText(txt)
---
+++ toki/sitelenpona/open.nim
@@ -0,0 +1,28 @@
+import nimib
+
+import ../../common/common
+
+nbInit
+
+nb.addCss(spCss)
+nb.darkMode()
+
+nbTextSp: """
+# lipu open
+
+mu a
+
+mi alasa sitelen pona lon lipu ni
+
+sitelen li ike la o toki e ike ona
+
+## toki mi
+
+mi kama sitelen pona e toki ali a mi
+
+o awen
+"""
+
+nb.filename = "toki/sitelenpona/index.html"
+nbSave
+
