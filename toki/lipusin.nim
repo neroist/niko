f29b98a80b9ffd0208c3b86b2d355b40f15d1a4f
neroist <pmpiga1@gmail.com>
2024-02-07 16:47:24 -0500

    lipu sin a

---
+++ toki/lipusin.nim
@@ -0,0 +1,47 @@
+import nimib
+
+nbInit 
+
+nb.darkMode()
+nb.title = "pali sin niko: lipu sin"
+
+nbText: """
+mu! mi toki lili taso lon ni
+
+kulupu mi la mute li kama pali e toki sin · ona li toki nasa a tawa mi li
+sama ala toki pona · ona li suli a · toki pona la nimi ~137 · toki ni la
+nimi ~500 · kin sina ken ante e kon nimi kepeken pana  sama ni **↓**
+
+| toki sin | toki pona |
+| -------- | --------- |
+| -mi      | mi        |
+| ryhu     | nasa      |
+
+ryhumi (nimi "mi" li tawa pini nimi · sina pana a e ona tawa pini)
+
+### ↓
+
+mi nasa
+
+---
+
+ijo li awen a pali e ona · pali li pini ala · tenpo kama weka
+a la pali li pini 
+
+tenpo suli la mi pali ala e toki · ijo ante a li pali li pana e pali tawa
+ona · taso ni li kama ante · mi kama pali · mi sona ala e toki e sona toki
+e nimi "Dative case" e nimi sama · taso a mi sona e ijo ante e ilo a · mi
+kama pali e lipu a tawa ilo Memrise tawa toki ni · kepeken lipu la ijo li
+ken kama sona e nimi toki · ni li suli tawa mi taaannn ijo a jdknfsj msaaa
+
+pali lipu la mi pali e lipu ante mute · ona li kepeken nasin CSV (`<nimi>.csv`)
+
+`[ken la tenpo kama la mi pana e ona]`
+
+taso sina ken ala lukin e lipu a >:3 · sina lon ala kulupu mi la ken ala
+a!
+
+> *:3c*
+"""
+
+nbSave
