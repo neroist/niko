94de9379e26b9a782c3ea3d94b6a6fed556c221e
neroist <pmpiga1@gmail.com>
2024-02-08 22:32:32 -0500

    mi lukin sitelen pona e lipu wan,,, ona o ike ala a o jaki ala

---
+++ toki/sitelenpona/lipusin.nim
@@ -0,0 +1,109 @@
+import nimib
+
+import ../../common/common
+
+nbInit 
+
+nb.addCss(spCss)
+nb.darkMode()
+nb.title = "pali sin niko: lipu sin"
+
+nbTextSp: """
+<!-- # pali sin niko: lipu sin -->
+
+mu! mi toki lili taso lon lipu ni
+
+kulupu mi la mute li kama pali e toki sin 
+
+ona li toki nasa a tawa mi 
+
+zzli sama ala toki pona 
+
+ona li suli a 
+
+toki pona la nimi 137 · toki ni^ la nimi 500 a 
+
+kin sina ken ante e kon nimi kepeken pana 
+
+#### sama niv
+
+|      toki sin      |      toki pona      |
+| ------------------ | ------------------- |
+| -YM                | mi (li)             |
+| -VIIZ              | lon tenpo pini poka |
+| EVELET             | lete                |
+
+> nimi「YM」en nimi「VIIZ」li tawa pini nimi 
+> 
+> sina pana a e ona tawa pini
+
+**EVELET**VIIZYM
+
+### niv
+
+mi lete lon tenpo pini poka
+
+---
+
+ijo li awen a pali e ona 
+
+pali li pini ala 
+
+tenpo kama weka a la pali li pini 
+
+tenpo suli la mi pali ala e toki 
+
+ijo ante a li pali 
+
+zzzzzzli pana e pali tawa ona
+
+taso ni^ li kama ante
+
+zzmi kama pali a
+
+mi sona ala e toki 
+
+zzzzzze sona toki
+
+zzzzzze nimi 「DATIVE CASE」
+
+zzzzzze nimi sama 
+
+taso a mi sona e ijo ante 
+
+zzzzzzzze ilo a 
+
+mi kama pali e lipu a tawa ilo「MEMRISE」
+
+zzzzzzzzzzzztawa toki ni^
+
+kepeken lipu la ijo li ken kama sona e nimi toki 
+
+zzni^ li suli tawa mi a
+
+pali lipu la mi pali e lipu ante mute
+
+zzzzzzzzzzzzzzzztawa pali
+
+ona li kepeken nasin CSV `<nimi>.csv`
+
+`[ken la tenpo kama la mi pana e ona]`
+
+kin ona li pona tawa akesi toki mi! · toki ona la lipu mi li pona a
+"""
+
+nbImage(
+  "images/tokiplalav.webp",
+  "",
+  "toki ona"
+)
+
+nbText: """
+taso sina ken ala lukin e lipu a `>:3`
+
+sina lon ala kulupu mi la ken ala a
+
+> *:3c*
+"""
+
+nbSave
