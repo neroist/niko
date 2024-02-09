0fbc03224e17663bc9c6a736a5464679ea54866d
neroist <pmpiga1@gmail.com>
2024-02-09 00:51:15 -0500

    mi sitelen pona e lipu [kala tonsi]

---
+++ toki/sitelenpona/kalatonsi.nim
@@ -0,0 +1,163 @@
+import nimib
+
+import ../../common/common
+
+nbInit 
+
+nb.addCss(spCss)
+nb.darkMode()
+nb.title = "kala li tonsi la"
+
+nbTextSp: """
+# kala li tonsi la
+
+<br>
+
+tenpo poka la mi tawa kulupu esun [ijo ken esun a]
+
+zzzzzzli tawa [lipu esun ona](https://www.ikea.com/us/en/)
+
+zzzzzzzzkepeken ilo mi
+
+lipu la mi pana e mani tawa kulupu lon esun 
+
+mi esun e seme
+
+zzmi pana e mani 
+
+zztaso seme li kama lon mi tan pana a
+
+o lukin a
+"""
+
+nbImage(
+  "https://raw.githubusercontent.com/neroist/poki/main/sitelen/blahaj2.png",
+  "kala a",
+  "sitelen ni la kala tonsi suwi pona wawa musi laso mu mi"
+)
+
+nbTextSp: """
+kala tonsi a!!
+
+kala tonsi a!!! 
+
+---
+
+ali la
+
+tenpo ali a la 
+
+zzzzzzmi wile2 e ona
+
+tenpo lon la ona li lon a mi a
+
+mi olin2 olin2 olin2 a e ona
+
+mi esun e mani [$30] anu mani sama tawa ona
+
+ona li suwi
+
+zzli pona
+
+zzli ko
+
+zzli olin2 mi
+
+mi lape la ona li lon luka mi li pilin e mi
+
+mi ken lape pona wawa a lon poka ona a
+
+---
+
+mi olin wawa suli a e ona 
+
+taso mi pilin monsuta
+
+zzzzzztan ijo poka a
+
+zzzzzztan ijo ike ken
+
+kala ni^ li lon meli2 tonsi mute mute mute
+
+ken la mama mi li sona e kala ni^ 
+
+zzzzzzzzzzzze tonsi ona
+
+zzzzzzzzzzzze meli2 ona 
+
+mama li pilin ike suli tawa meli2
+
+zzzzzzzzzztawa tonsi
+
+ona li sona la
+
+zzken la ona li weka anu pakala e kala
+
+zzzzni>^ li ike ali
+
+---
+
+ijo sin a niv
+
+zzlukin la ona li sona ala e meli2 e tonsi 
+
+zzzzzzzzli pakala ala e kala
+
+zzni>^ li pona mute a
+
+---
+
+kin sona sin niv li kama lon mi 
+
+zzzzzzzztenpo la kala li ken kama lili sama [sitelen ni<](https://raw.githubusercontent.com/neroist/poki/53f83c7db482fa1e5f123297a6cae20f1714fb83/sitelen/flathaj.jpg)
+
+zzni^> li ike tawa mi o
+
+ona o awen suli a
+
+mi wile2 ala a e ona lili 
+
+kin ona li kama lili la mi ken suli sin e ona 
+
+zzzzzzzzzzzzzzzzzzzzzzkepeken nasin seme a
+
+jan li suli e kala ona
+
+zzzzzzzzzzkepeken ko walo
+
+zzzzzzzzzzkepeken ilo ante kin
+
+taso ijo ni^ ali li lon ala mi a 
+
+<!-- zzzzzzzzli ken ala lon mi -->
+
+ken suli la
+
+zzzzmi lukin alasa e ijo ni^
+
+zzzzzzzzzze ilo
+
+zzzzzzzzzze ko la
+
+zzzzzzzzzzzzzzona li awen weka
+
+zzzzzzzzzzzzzzzzli kama ala lon mi a
+
+ni^> li ike suli
+
+kin kala li kama lili la 
+
+zzpona lape kin li kama lili
+
+tan ijo-tu ni^ la mi pilin monsuta
+
+taso a
+
+zzmi awen olin wawa e kala tonsi mi 
+
+mu
+
+> *:3c*
+"""
+
+nbSave
